fa081dff5c70f38f7ab1bd96c37f9b07be425950
lvivduncan <lvivduncan@gmail.com>
2020-01-25 19:25:46 +0200

    Update levus-dropdown-menu.js

--- js/levus-dropdown-menu.js
+++ js/levus-dropdown-menu.js
@@ -1,1 +1,42 @@
+// debug
+const echo = console.log;
 
+{
+    window.addEventListener('resize', toggleMobileMenu);
+
+    // отримуємо нашу гарну менюшечку
+    const levusMenu = document.querySelector('.levus-menu');
+
+    // додаємо кнопку коло парента
+    levusMenu.querySelectorAll('.parent').forEach(item => {
+        const el = document.createElement('span');
+        item.appendChild(el);
+    });
+
+    // викликаємо функцію, щоб зміна відбувалася не тільки при ресайзі, а й при оновленні сторінки
+    toggleMobileMenu();
+
+    // відкриваємо/закриваємо мобільне меню
+    document.addEventListener('click', e => {
+        if (e.target.classList == 'levus-menu levus-menu-mobile') {
+            document.querySelector('.levus-menu ul').classList.toggle('block');
+        }
+    });
+
+    // розкриваємо блоки у підпунктів
+    levusMenu.addEventListener('click', e => {
+        if (e.target.tagName == 'SPAN') {
+            // 2 елемент (ul)
+            e.target.parentNode.children[1].classList.toggle('open');
+        }
+    });
+
+    // вмикаємо/вимикаємо мобільне меню
+    function toggleMobileMenu(){       
+        if (window.innerWidth < 1250) {
+            levusMenu.classList.add('levus-menu-mobile');
+        } else {
+            levusMenu.classList.remove('levus-menu-mobile');
+        }
+    }
+}
